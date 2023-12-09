79ac8a6e7c297fd27ec7f6f7ccb02d791f4a94e8
Gustavo Ferreira <gustavo.ferreira@imaginecurve.com>
2023-12-09 16:05:51 +0000

    add code for day 9

---
+++ advent_of_code/2023/day09/part1/solutions/solution_v1.go
@@ -0,0 +1,135 @@
+package solutions
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Solver struct {
+	debug        bool
+	inputContent []string
+	result       int
+
+	// each line represents a value changing over time
+	puzzle [][]int
+}
+
+func NewSolver() *Solver {
+	return &Solver{}
+}
+
+func (s *Solver) SetDebug(enable bool) {
+	s.debug = enable
+}
+
+// LoadArrayOfLines loads the array in the internal representation of the input content.
+func (s *Solver) LoadArrayOfLines(lines []string) {
+	for _, line := range lines {
+		s.LoadLine(line)
+	}
+}
+
+// LoadLine loads the input line into the internal representation of the input content.
+func (s *Solver) LoadLine(line string) {
+	s.inputContent = append(s.inputContent, line)
+}
+
+// ComputeResult computes the result and stores it in the result field.
+func (s *Solver) ComputeResult() {
+	// parse input
+	itemIndex := 0
+
+	for _, line := range s.inputContent {
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+
+		items := strings.Fields(line)
+
+		var numbersLine []int
+
+		for _, item := range items {
+			number, _ := strconv.Atoi(item)
+			numbersLine = append(numbersLine, number)
+		}
+
+		s.puzzle = append(s.puzzle, numbersLine)
+		itemIndex++
+	}
+
+	if s.debug {
+		fmt.Println("Input:")
+
+		for _, line := range s.puzzle {
+			fmt.Printf("%+v\n", line)
+		}
+		fmt.Println("----------")
+	}
+
+	result := 0
+
+	// Work out prediction for each line
+	for _, line := range s.puzzle {
+		prediction := s.predictNextNumber(line)
+		result += prediction
+	}
+
+	s.result = result
+}
+
+// Result returns the current result stored in the Solver.
+func (s *Solver) Result() int {
+	// Not exporting the variable makes sure the user only gets read-only access to the underlying field.
+	return s.result
+}
+
+func (s *Solver) predictNextNumber(line []int) int {
+	result := line[len(line)-1]
+
+	finished := false
+	currentLine := line
+	var intermediaryLines [][]int
+
+	if s.debug {
+		fmt.Printf("Computed prediction for: %+v\n", line)
+	}
+
+	for !finished {
+		var resultLine []int
+		diffThanZero := false
+
+		for i := 0; i < len(currentLine)-1; i++ {
+			diff := currentLine[i+1] - currentLine[i]
+			resultLine = append(resultLine, diff)
+			if diff != 0 {
+				diffThanZero = true
+			}
+		}
+
+		currentLine = resultLine
+		intermediaryLines = append(intermediaryLines, resultLine)
+
+		if s.debug {
+			fmt.Printf("Iteration line result: %+v\n", resultLine)
+		}
+
+		// decide whether we are done
+		if !diffThanZero {
+			finished = true
+		}
+	}
+
+	// compute prediction
+	for i := len(intermediaryLines) - 1; i >= 0; i-- {
+		lastIndex := len(intermediaryLines[i]) - 1
+		result += intermediaryLines[i][lastIndex]
+	}
+
+	if s.debug {
+		fmt.Printf("Prediction: %d\n", result)
+	}
+
+	return result
+}
---
+++ advent_of_code/2023/day09/part2/cmd/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/gustavooferreira/coding/advent_of_code/2023/day09/part2/solutions"
+)
+
+// Run example:
+// > cat solutions/testdata/input2.txt | go run cmd/main.go
+func main() {
+	debugFlag := flag.Bool("debug", false, "print debug messages")
+	flag.Parse()
+
+	solver := solutions.NewSolver()
+	solver.SetDebug(*debugFlag)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+		solver.LoadLine(line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Error: error while scanning the file: %s\n", err)
+		os.Exit(1)
+	}
+
+	solver.ComputeResult()
+
+	if *debugFlag {
+		fmt.Println("--------------------")
+	}
+
+	fmt.Printf("Result: %d\n", solver.Result())
+}
